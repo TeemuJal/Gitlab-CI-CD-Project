5d4bc0446b2a7adde1438bc7aab7ef6e051fad75
TeemuJal <32645397+TeemuJal@users.noreply.github.com>
2020-11-30 17:23:37 +0200

    Changed http-service's test to be simpler

--- tests/http-service/app.test.js
+++ tests/http-service/app.test.js
@@ -1,25 +1,11 @@
-const rewire = require("rewire");
-const app = rewire("../../http-service/app.js");
+const app = require("../../http-service/app.js");
 const supertest = require("supertest");
+const server = supertest(app);
 
-describe("e2e tests", () => {
-  let server;
-  beforeAll(async () => {
-    server = supertest(app.__get__("server"));
-    await sleep(4000);
-  });
+test("Test http-service returns messages in the correct format", async done => {
+  const res = await server.get("/");
 
-  test("Test http-service with and e2e test", async done => {
-    const res = await server.get("/");
-
-    expect(res.status).toBe(200);
-    expect(res.text.replace(/"([^"]+(?="))"/g, '$1')).toMatch(new RegExp(/^((?:\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z) Topic my\.(o|i): ?(Got)? MSG_\d+\n?){6})$/));
-    done();
-  });
+  expect(res.status).toBe(200);
+  expect(res.text.replace(/"([^"]+(?="))"/g, '$1')).toMatch(new RegExp(/^((?:\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z) Topic my\.(o|i): ?(Got)? MSG_\d+\n?){6})$/));
+  done();
 });
-
-function sleep(ms) {
-  return new Promise((resolve) => {
-      setTimeout(resolve, ms);
-  });
-}  
