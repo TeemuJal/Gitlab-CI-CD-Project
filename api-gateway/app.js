e5fdddb57ef43d5a8491166010a841779c8f5acf
TeemuJal <32645397+TeemuJal@users.noreply.github.com>
2020-12-02 00:46:33 +0200

    Implemented API gateway's GET /messages endpoint

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const app = express()
+const app = express();
+const axios = require("axios");
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get("/messages", async function (req, res) {
+  try {
+    const messages = await axios.get("http://httpserv:8082/");
+    res.send(messages.data);
+  } catch (error){
+    console.error(error);
+    res.status(500).send("Something went wrong.");
+  } 
+});
 
 module.exports = app;
