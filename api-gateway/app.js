6a6c2f0cdc7e73255871f0da0fff83dc77cad139
TeemuJal <32645397+TeemuJal@users.noreply.github.com>
2020-12-03 01:12:08 +0200

    Implemented PUT /state/PAUSED functionality

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -2,13 +2,38 @@ const express = require("express");
 const app = express();
 const axios = require("axios");
 
+const states = {
+  PAUSED: "PAUSED"
+};
+
+// Endpoint for getting all registered messages
 app.get("/messages", async function (req, res) {
   try {
     const messages = await axios.get("http://httpserv:8082/");
     res.send(messages.data);
-  } catch (error){
+  } catch (error) {
     console.error(error);
     res.status(500).send("Something went wrong.");
+  } 
+});
+
+// Endpoint for changing the state of the system
+app.put("/state/:new_state", async function (req, res) {
+  const new_state = req.params.new_state;
+  // Check if given state is a valid state
+  if (Object.values(states).indexOf(new_state) > -1) {
+    switch(new_state) {
+      case states.PAUSED:
+        try {
+          const response = await axios.post("http://orig:5000/pause");
+          res.send(response.data);
+        } catch (error) {
+          console.error(error);
+          res.status(500).send("Something went wrong.");
+        } 
+    }
+  } else {
+    res.status(400).send("Invalid state.");
   } 
 });
 
--- api-gateway/start.js
+++ api-gateway/start.js
@@ -1,4 +1,4 @@
 const app = require("./app")
 const port = 8081;
 
-app.listen(8081);
+app.listen(port);
