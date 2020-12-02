677c2c6caaa01bb2bab3ce79f0cb909289bb82f7
TeemuJal <32645397+TeemuJal@users.noreply.github.com>
2020-12-02 15:34:31 +0200

    Reworked ORIG service to send messages until paused

--- original/app.js
+++ original/app.js
@@ -1,24 +1,21 @@
-var mqPublish = require('./rabbit-utils/publishMsg.js');
+const express = require("express");
+const app = express();
+
+const mqPublish = require('./rabbit-utils/publishMsg.js');
 const topic = "my.o";
 
-publishThreeMessagesToQueue();
+let interval_id = setInterval(publishMessageToQueue, 3000);
+let msg_counter = 0;
 
-async function publishThreeMessagesToQueue() {
-    await sleep(3000);
-    const msg1 = "MSG_1";
-    mqPublish.publishMsg("guest:guest@rabbitmq", "messages", topic, msg1);
+// Endpoint for pausing the ORIG service to stop sending messages
+app.post("/pause", function (req, res){
+    clearInterval(interval_id);
+    res.status(200).send("ORIG paused");
+});
 
-    await sleep(3000);
-    const msg2 = "MSG_2";
-    mqPublish.publishMsg("guest:guest@rabbitmq", "messages", topic, msg2);
-
-    await sleep(3000);
-    const msg3 = "MSG_3";
-    mqPublish.publishMsg("guest:guest@rabbitmq", "messages", topic, msg3);
+// Publish a message to the queue
+function publishMessageToQueue() {
+    msg_counter += 1;
+    const msg = `MSG_${msg_counter}`;
+    mqPublish.publishMsg("guest:guest@rabbitmq", "messages", topic, msg);
 }
-  
-function sleep(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-    });
-}  
