6c8145c1042da1917bce71708e2480e686e2df2b
TeemuJal <32645397+TeemuJal@users.noreply.github.com>
2020-12-02 15:35:54 +0200

    Updated HTTPSERV test to allow unlimited messages instead of the strict six messages

--- tests/http-service/app.test.js
+++ tests/http-service/app.test.js
@@ -6,6 +6,6 @@ test("Test http-service returns messages in the correct format", async done => {
   const res = await server.get("/");
 
   expect(res.status).toBe(200);
-  expect(res.text.replace(/"([^"]+(?="))"/g, '$1')).toMatch(new RegExp(/^((?:\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z) Topic my\.(o|i): ?(Got)? MSG_\d+\n?){6})$/));
+  expect(res.text.replace(/"([^"]+(?="))"/g, '$1')).toMatch(new RegExp(/^((?:\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z) Topic my\.(o|i):( Got)? MSG_\d+\n?)+)$/));
   done();
 });
