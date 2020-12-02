507539984b86ebbfee5672824464c7ccf7ed88cc
TeemuJal <32645397+TeemuJal@users.noreply.github.com>
2020-12-03 00:24:11 +0200

    Updated ORIG-service to use a start.js for starting the server

--- original/app.js
+++ original/app.js
@@ -7,6 +7,12 @@ const topic = "my.o";
 let interval_id = setInterval(publishMessageToQueue, 3000);
 let msg_counter = 0;
 
+// Endpoint for starting the ORIG service to send messages
+// app.post("/start", function (req, res){
+//     interval_id = setInterval(publishMessageToQueue, 3000);
+//     res.status(200).send("ORIG running");
+// });
+
 // Endpoint for pausing the ORIG service to stop sending messages
 app.post("/pause", function (req, res){
     clearInterval(interval_id);
@@ -19,3 +25,5 @@ function publishMessageToQueue() {
     const msg = `MSG_${msg_counter}`;
     mqPublish.publishMsg("guest:guest@rabbitmq", "messages", topic, msg);
 }
+
+module.exports = app;
