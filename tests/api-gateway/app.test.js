2936d3da4234b7e9ed4b408f0da6cc31f1be0145
TeemuJal <32645397+TeemuJal@users.noreply.github.com>
2020-12-02 14:00:31 +0200

    Added test for PUT /state endpoint with PAUSED payload

--- tests/api-gateway/app.test.js
+++ tests/api-gateway/app.test.js
@@ -5,6 +5,7 @@
 const app = require("../../api-gateway/app.js");
 const supertest = require("supertest");
 const server = supertest(app);
+const fs = require("fs");
 
 describe("GET /messages", () =>{ 
   test("Test that endpoint returns all messages registered with OBSE-service", async done => {
@@ -15,3 +16,43 @@ describe("GET /messages", () =>{
     done();
   });
 });
+
+describe("PUT /state", () =>{ 
+  test("Test setting ORIG service's state to PAUSED", async done => {
+    // Set ORIG service to not send anymore messages
+    const res = await server.put("/state/:new_state").send("PAUSED");
+    expect(res.status).toBe(200);
+    
+    console.log("ORIG set to PAUSED - waiting 10s");
+    // Wait for currently queued messages to be registered
+    await sleep(10000);
+
+    console.log("Reading messages from file 1st time...");
+    // Fetch messages from the file
+    let original_messages;
+    fs.readFileSync("/var/lib/messages/messages.txt", function(err, data) {
+      original_messages = data;
+    });
+
+    console.log("1st messages read - waiting 5s for new messages");
+    // Wait to see if new messages are registered
+    await sleep(5000);
+
+    console.log("Reading messages from file 2nd time...");
+    // Fetch messages from the file
+    let new_messages;
+    fs.readFileSync("/var/lib/messages/messages.txt", function(err, data) {
+      new_messages = data;
+    });
+
+    // Check responses are the same i.e. setting ORIG to PAUSED state was successful
+    expect(new_messages).toBe(original_messages);
+    done();
+  });
+});
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+  });
+}  
